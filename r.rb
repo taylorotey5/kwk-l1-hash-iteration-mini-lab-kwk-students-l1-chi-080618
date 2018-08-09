7a677d71223708997f507f0caa7bfb02deb55dbb
Taylor Otey <taylorotey5@gmail.com>
2018-08-09 18:51:11 +0000

    automatically backed up by learn

---
+++ r.rb
@@ -0,0 +1,30 @@
+def type_1
+  puts "give me a verb ends in ing"
+  verbing1 = gets.chomp
+  puts "give a name"
+  name1 = gets.chomp
+  puts "give me a place"
+  place1 = gets.chomp
+  puts "give me an adjective"
+  adjective1 = gets.chomp
+  puts "give me a plural noun"
+  pluralnoun1 = gets.chomp
+  puts "give me a plural noun"
+  pluralnoun2 = gets.chomp
+  puts "give me a noun"
+  noun1 = gets.chomp
+  puts "give me a verb ends in ing"
+  verbing2 = gets.chomp
+  puts "give me an adjective"
+  adjective2 = gets.chomp
+  puts "give me a noun"
+  noun2 = gets.chomp
+  puts "give me an adjective"
+  adjective3 = gets.chomp
+puts "So I was #{verbing1} and loving life. My name is #{name1} and I live in #{place1} in the middle of no where. There are #{adjective1} and small cows that live in my house. I wish I had #{pluralnoun1} and #{pluralnoun2}. I play with my #{noun1} and enjoy #{verbing2}. I want to move to the middle of the ocean and live in a #{adjective2} boat. Anway I love life eating my #{noun2}. Anyway see you next time and have a #{adjective3} life!"
+end
+
+type_1
+
+
+
--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,18 +1,37 @@
 
 def create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
+  olympics_hash = {
+    :Sydney => "2000",
+    :Athens => "2004",
+    :Beijing => "2008",
+    :London => "2012"
+  }
+  return olympics_hash
 end
 
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+  updated_hash = create_olympics_hash
+  updated_hash[:Atlanta] = "1996"
+  return updated_hash
 end
+
 
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+  iterate = add_a_key_value_pair
+  iterate.each do |place, year|
+  puts "The #{place} summer olympics took place in #{year}."
+  end
 end
 
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
+  iterate_through_keys = 
+  .each do |place|
+  puts
+  end
 end
